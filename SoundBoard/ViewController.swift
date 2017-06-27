a2332dede27a4ff75def3609fb835430d7da38c9
Madiana Hedayet <madianah@gmail.com>
2017-06-27 16:58:00 -0700

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Madiana Hedayet on 6/27/17.
+//  Copyright © 2017 Madiana Hedayet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
